7cf5b029efa047e838a217e542e87cdc9a5d0dab
Sweta Tara Tuladhar <taratuladharsweta@gmail.com>
2023-09-17 09:43:52 +0545

    edit profile,ProfilePic,email function left

--- vehicle_servicing/urls.py
+++ vehicle_servicing/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from vehicle import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     # path("send_email/",views.send_email, name='send_email'),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
